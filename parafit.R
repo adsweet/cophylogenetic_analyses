19e6affb4e6a331581c9bed176f6431628bb3ca7
Drew <Sweet@nrb-dhcp-47.inhs.illinois.edu>
2016-05-11 12:09:44 -0500

    ParaFit code

--- parafit.R
+++ parafit.R
@@ -1,18 +1,16 @@
-###CODE FOR BRUEELIA PARAFIT ANALYSIS
+###R CODE FOR PARAFIT ANALYSIS
 
 library(ape)
 
 #READ IN PARASITE TREE
 p.tree <- read.nexus('Brueelia_MCC.tre')
 
-#tree <- read.tree('Brueelia_bGMYC_random100.trees')
-
 #ROOT PARASITE TREE
 p.root <-  root(p.tree, c("Chsp.Orcan.11.10.2001.9","Oxchi.1.27.1999.6"))
 
 #REMOVE PARASITE OUTGROUPS
 p.tree.noout <- drop.tip(p.root, c("Pasp.Arast.2.10.1999.7", "Pclau.11.22.2001.13","Tssp.Psbre.10.16.2002.6","Ffpal.11.22.2001.14","Qkeos.5.16.2002.5","Nylon.2.6.1999.6","Racol.1.27.1999.2","Raful.2.6.1999.11","Alsp.Hamal.1.16.2001.11","Veber.10.17.2000.7","Qupun.2.3.1999.2","Salar.4.7.1999.12","Afdup.3.16.2001.10","Oscur.10.5.1999.2","Wiabs.10.5.1999.1","Cabid.6.29.1998.2","Phcub.9.29.1998.7","Gosp.Phcol.11.10.2001.2","Sgorb.11.10.2001.10","Brsp.Psmin.2.1.2000.9","Pezum.1.12.1999.10","Ppsp.Sppus.11.10.2001.11","Ausub.1.27.1999.12","Dgcar.9.8.1999.7","Embra.2.4.2002.11","Chsp.Orcan.11.10.2001.9","Oxchi.1.27.1999.6","Nseos.11.22.2001.15","Foana.1.27.1999.7","Fosp.Thdol.4.7.1999.10"))
-
+is.rooted(p.tree.noout)
 
 #REMOVE DUPLICATE TAXA 1
 p.trees.trim1 <- drop.tip(p.tree.noout, c("Brsp.Sphsp.12.11.2011.14","Brsp.Hecas.12.11.2011.4","Brsp.Hypmcc.12.11.2011.6","Brsp.Hirus.4.16.2013.6","Brgr.2.9.2011.37","Brgra.Deurb.1.23.2012.3","Brsp.Hidau.10.25.2011.9","Brsp.Cahae.10.12.1999.9","Brsp.Door.6.13.2006.13","BrspAgpho326201238B","Bror.8.29.2011.16","Bror.8.29.2011.10","BrspIcgal326201240","Brsp.Moate.3.24.2001.3","Brxan.Xaxan.10.30.2012.5","Brsp.Lapur.8.31.2011.7","Brsp.Euar.1.25.2011.5","Brsp.Mecru.11.28.2011.1","BrspStmag326201247","Brsp.Pail.5.30.2012.17","Brvu.6.27.2006.28","Brsp.Meme.8.29.2011.8","Brsp.Amne.8.16.2011.3","Brsp.Spar.9.14.2011.3","Brsp.Zoal.6.27.2006.19","Brst.8.29.2011.15","Brbla.Emsch.1.23.2012.4","Brsp.Getr.8.16.2011.13","Brsp.Seau.9.14.2011.1","Bril.5.30.2012.19","BrspApcal32620122","Brili.Tumig.10.30.2012.8","Brsp.Seca.2.9.2011.29","Brsp.Capi.8.16.2011.6","Brsp.Plru.11.15.2010.17","Brsp.Panig.1.12.1999.11","Brsp.Pesu.2.9.2011.38","Brsp.Larh.5.30.2012.5","BrspLoleu42820125","BrspLostr326201233","Brsp.Crre.11.29.2010.16","Brsp.Orau.11.29.2010.15","Brsp.Orau.4.5.2011.3","Brsp.Orla.5.30.2012.18","Brsp.Spvir.12.13.2011.12","Brsp.Cipun.12.16.2011.4","Brsp.Comel.11.8.2011.1","BrspMefra420201219","Pnsp.Memys.1.15.2000.15","Stsp.Tuli.12.21.2011.24","Snsim.Tumig.8.12.2013.10","Stsp.Negu.12.21.2011.13","Brsp.Tuchr.1.23.2012.6","Brsp.Cafr.6.13.2006.4","Stsp.Hymu.6.13.2006.11","Stsp.Hymu.6.13.2006.8","Stsp.Euar.12.21.2011.8","Stsp.Euar.5.30.2012.12","Stsp.Eual.12.21.2011.6"))
@@ -35,19 +33,19 @@ h.tree.root <- root(h.tree, c("Gallus_gallus"))
 h.tree.noout <- drop.tip(h.tree.root, c('Gallus_gallus'))
 is.rooted(h.tree.noout)
 h.tree.noout
+#plot(h.tree.noout)
 
 #CALCULATE PATRISTIC DISTANCES FOR HOST TREE
-h.pats <- as.matrix(cophenetic(h.tree))
+h.pats <- as.matrix(cophenetic(h.tree.noout))
 
 #MATRIX
 brueelia_mx <- as.matrix(read.table('HP_Matrix.txt', header=T))
+#REORDER DISTANCE MATRICES ACCORDING TO THE HP MATRIX
 h.pats <- h.pats[rownames(brueelia_mx), rownames(brueelia_mx)]
 p.pats <- p.pats[colnames(brueelia_mx), colnames(brueelia_mx)]
-#CONVERT TO A MATRIX
-#brueelia_mx <- as.matrix(brueelia_mx)
 
 #Run ParaFit
-#brpf <- parafitparafit(h.pat, p.pat, brueelia_mx, nperm=1, test.links=TRUE, correction='lingoes')
+#brpf <- parafitparafit(h.pats, p.pats, brueelia_mx, nperm=999, test.links=TRUE, correction='lingoes')
 #Alternative correction
 brpf <- parafit(h.pats, p.pats, brueelia_mx, nperm=999, test.links=T, correction='cailliez')
 
@@ -55,6 +53,4 @@ brpf <- parafit(h.pats, p.pats, brueelia_mx, nperm=999, test.links=T, correction
 HP.LUT <- which(brueelia_mx ==1, arr.in=TRUE)
 links <- cbind(rownames(brueelia_mx)[HP.LUT[,1]],colnames(brueelia_mx)[HP.LUT[,2]])
 par(mar=c(1,1,1,1))
-cophyloplot(h.tree, p.tree, assoc=links, 
-            show.tip.label=F, # too long taxa names...
-            space=400, use.edge.length=FALSE, gap=0.5, rotate=T, col='red')
+cophyloplot(h.tree, p.tree, assoc=links, show.tip.label=F, space=400, use.edge.length=FALSE, gap=0.5, rotate=T, col='red')
