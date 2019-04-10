b35eb27be48bc8460b1c7421166b1193d4c0846f
Drew Sweet <dulcisad@gmail.com>
2019-04-10 15:20:19 -0400

    Update make_tangle.py

--- make_tangle.py
+++ make_tangle.py
@@ -16,27 +16,23 @@ parser.add_argument("--parasite","-p", help="Input parasite tree", required=True
 parser.add_argument("--output","-o", help="Output tanglegram file", required=True)
 args = parser.parse_args()
 
-
-
 matrix = pd.read_csv(args.matrix)
 host_tree_f = open(args.host,'r')
 host_tree = host_tree_f.read()
-host_tree_trim1 = re.sub(r":\d\.\d+",r"",host_tree)
-host_tree_trim = re.sub(r"\)\d+",r")", host_tree_trim1)
+host_tree_trim1 = re.sub(r":\d\.\d+",r"",host_tree) # Remove branch lengths in host tree file
+host_tree_trim = re.sub(r"\)\d+",r")", host_tree_trim1) # Remove branch support in host tree file
 para_tree_f = open(args.parasite,'r')
 para_tree = para_tree_f.read()
-para_tree_trim1 = re.sub(r":\d\.\d+",r"",para_tree)
-para_tree_trim = re.sub(r"\)\d+",r")", para_tree_trim1)
-#print(len(matrix.columns))
+para_tree_trim1 = re.sub(r":\d\.\d+",r"",para_tree) # Remove branch lengths in parasite tree file
+para_tree_trim = re.sub(r"\)\d+",r")", para_tree_trim1) # Remove branch support in parasite tree file
 with open(args.output,"w") as out:
-	out.write('#NEXUS' + '\n' + 'BEGIN HOST;' + '\n' + '\t' + 'TREE * Host1 = ' + host_tree_trim + 'ENDBLOCK;' + '\n' + 'BEGIN PARASITE;' + '\n' + '\t' + 'TREE * Para1 = ' + para_tree_trim + 'ENDBLOCK;' + '\n')
+	out.write('#NEXUS' + '\n' + 'BEGIN HOST;' + '\n' + '\t' + 'TREE * Host1 = ' + host_tree_trim + 'ENDBLOCK;' + '\n' + 'BEGIN PARASITE;' + '\n' + '\t' + 'TREE * Para1 = ' + para_tree_trim + 'ENDBLOCK;' + '\n') # Write trees to outfile
 	out.write('BEGIN DISTRIBUTION;' + '\n' + '\t' + 'RANGE' + '\n')
 	for (index, row) in matrix[0:len(matrix.index)].iterrows():
 		for i in range(len(matrix.columns)):
 			if row[i] == 1:
-				#print(row.index[i])
 				h_val = row.name
 				louse = row.index[i]
 				host = matrix.iloc[h_val,0]
-				out.write('\t' + '\t' + louse + ':' + host + ',' + '\n')
-	out.write('\t' + ';' + '\n' + 'END;')
+				out.write('\t' + '\t' + louse + ':' + host + ',' + '\n') # Write associations to outfile
+	out.write('\t' + ';' + '\n' + 'END;') # Write end of file to outfile
